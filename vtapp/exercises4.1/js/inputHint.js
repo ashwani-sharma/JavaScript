239cb49e3f00b20e73ed8ed05b51a2cd221a19d4
Ashwani Sharma <sharma.ashwani007@gmail.com>
2013-07-25 11:02:22 +0530

    exercise 4.1 resubmitted

--- vtapp/exercises4.1/js/inputHint.js
+++ vtapp/exercises4.1/js/inputHint.js
@@ -10,49 +10,11 @@ $(function(){
 		if($(this).val() == labelVal){
 			$(this).val("").removeClass("hint");
 		}
-	});
-	
-	$(formInput).bind("blur", function(){
+	}).bind("blur", function(){
 		var currentInputVal = formInput.val();
 		currentInputVal = jQuery.trim(currentInputVal);
 		if(currentInputVal == ""){
 			$(formInput).val(labelVal).addClass("hint");
 		}
 	});
-	
-	
-	
-	
-	var findLi = $("div#tabs ul li");
-	var findDiv = $("div#tabs-container div.content");
-	var findDivId = $("div#tabs-container div.content").attr("id");
-	var currentLi = $("div#tabs ul li.current");
-	
-	$(document).ready(function(){
-		$(findLi).first().addClass("current");
-		$(findDiv).hide();
-		$(findDiv).first().show();
-		
-		$(findLi).each(function(){
-			$(this).bind("click", function(){
-				$(this).addClass("current").siblings().removeClass("current");
-			});
-		});
-	});
-	
-	
-	
-//	$(findLi).each(function(){
-//		$(this).bind("click", function(){
-//			$(this).addClass("current").siblings().removeClass("current");
-//		});
-//	});
-	
-//	$(findDiv).each(function(){
-//		if($(this).attr("id") == $(findLi).attr("rel")){
-//			$(this).css("display", "block").siblings().css("display", "none");
-//		}
-//	});
-	
-	
 });
