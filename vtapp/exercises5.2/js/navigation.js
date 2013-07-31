65c0071f95dbf7ce71ccd580bed1e42e70e2e9e1
Ashwani Sharma <sharma.ashwani007@gmail.com>
2013-07-31 16:48:46 +0530

    created blank navigation.js

--- vtapp/exercises5.2/js/navigation.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  var findHeading = $("#blog h3");
-	
-	$(findHeading).click(function(x){
-    x.preventDefault();
-		$(this).next("p").slideToggle().parent().siblings().find("p:visible").slideUp();
-	});
-});
