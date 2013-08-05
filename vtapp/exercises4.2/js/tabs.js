64a3f035128b344c1e893ff1436c98eda89b3e11
Ashwani Sharma <sharma.ashwani007@gmail.com>
2013-08-05 15:06:09 +0530

    resubmitted exercise 4.2

--- vtapp/exercises4.2/js/tabs.js
+++ vtapp/exercises4.2/js/tabs.js
@@ -6,30 +6,12 @@ $(document).ready(function(){
     var h2Text = $(this).children("h2").text();
     $("<li></li>").text(h2Text).attr("rel","#"+h2Text).appendTo("ul.tabs");
   });
-	
-  $("ul.tabs").css({
-    width : "100%",
-    float : "left",
-    listStyle : "none",
-    margin : "20px 20px 20px 0px"
-	});
-	
-	$("ul.tabs li").each(function(){
-    $(this).css({
-      float : "left",
-      margin : "0 0 0 10px",
-      fontFamily : "arial",
-      color : "blue",
-      listStyle : "none",
-      cursor : "pointer"
-    });
-  });
-	
-  $("ul.tabs li").first().addClass("current").css("color", "red").css("marginLeft", "0px");
+
+  $("ul.tabs li").first().addClass("current");
 	
   $("ul.tabs li").each(function(){
     $(this).bind("click", function(){
-      $(this).addClass("current").css("color", "red").siblings().removeClass("current").css("color", "blue");
+      $(this).addClass("current").siblings().removeClass("current");
 			
       var findRel = $(this).attr("rel").toLowerCase();
       $("div.module").hide();
